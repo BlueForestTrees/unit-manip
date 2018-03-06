0d230b1d834284691a117c2f9e570622d2b927e1
slimane <smedini@gmail.com>
2018-03-06 19:18:37 +0100

    common en cours

--- dist/units.js
+++ dist/units.js
@@ -3,13 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.initUnits = undefined;
+exports.bestQuantity = exports.toBaseQuantity = exports.qtUnitCoef = exports.unitCoef = exports.sameGrandeur = exports.grandeur = exports.unitlongname = exports.base = exports.coef = exports.unit = exports.getShortnames = exports.getGrandeursKeys = exports.getGrandeurs = exports.getUnits = exports.initUnits = undefined;
 
 var _lodash = require('lodash');
 
 var _lodash2 = _interopRequireDefault(_lodash);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var data = null;
+var units = null;
+var grandeurs = null;
 
 var initUnits = exports.initUnits = function initUnits(grandeurs) {
 
@@ -20,12 +24,105 @@ var initUnits = exports.initUnits = function initUnits(grandeurs) {
         grandeurs[grandeurName] = _lodash2.default.sortBy(units, 'coef');
     });
 
-    var units = _lodash2.default.chain(grandeurs).values().flatten().keyBy('shortname').value();
+    units = _lodash2.default.chain(grandeurs).values().flatten().keyBy('shortname').value();
 
-    return {
+    undefined.grandeurs = grandeurs;
+
+    data = {
         units: units,
         grandeurs: grandeurs,
         grandeursKeys: Object.keys(grandeurs),
         shortnames: Object.keys(units)
     };
+};
+
+var getUnits = exports.getUnits = function getUnits() {
+    return data.units;
+};
+var getGrandeurs = exports.getGrandeurs = function getGrandeurs() {
+    return data.grandeurs;
+};
+var getGrandeursKeys = exports.getGrandeursKeys = function getGrandeursKeys() {
+    return data.grandeursKeys;
+};
+var getShortnames = exports.getShortnames = function getShortnames() {
+    return data.shortnames;
+};
+
+var unit = exports.unit = function unit(shortname) {
+    return _lodash2.default.has(units, shortname) ? units[shortname] : null;
+};
+var coef = exports.coef = function coef(shortname) {
+    return unit(shortname).coef;
+};
+var base = exports.base = function base(grandeur) {
+    return _lodash2.default.find(grandeurs[grandeur], { coef: 1 });
+};
+
+var unitlongname = exports.unitlongname = function unitlongname(shortname) {
+    return unit(shortname).name;
+};
+var grandeur = exports.grandeur = function grandeur(shortname) {
+    return unit(shortname).grandeur;
+};
+
+/**
+ * @returns faux, ou vrai ssi les unités sont valides et de la même grandeur
+ */
+var sameGrandeur = exports.sameGrandeur = function sameGrandeur(leftShortname, rightShortname) {
+    var leftUnit = unit(leftShortname);
+    var rightUnit = unit(rightShortname);
+
+    return leftUnit && rightUnit && leftUnit.grandeur === rightUnit.grandeur;
+};
+
+/**
+ * @returns le coef pour passer d'une unité à l'autre. undefined si les unités ne sont pas compatibles.
+ */
+var unitCoef = exports.unitCoef = function unitCoef(leftShortname, rightShortname) {
+    return sameGrandeur(leftShortname, rightShortname) ? Fraction(unit(leftShortname).coef).div(unit(rightShortname).coef).valueOf() : undefined;
+};
+
+/**
+ * @returns le coef pour passer d'une quantité à l'autre. undefined si les unités ne sont pas compatibles.
+ */
+var qtUnitCoef = exports.qtUnitCoef = function qtUnitCoef(leftQuantity, rightQuantity) {
+    return leftQuantity && rightQuantity ? Fraction(leftQuantity.qt).div(rightQuantity.qt).mul(unitCoef(leftQuantity.unit, rightQuantity.unit)).valueOf() : undefined;
+};
+
+/**
+ *
+ * @param quantity
+ * @returns la quantité en unité de base. (10kg => 10000g)
+ */
+var toBaseQuantity = exports.toBaseQuantity = function toBaseQuantity(quantity) {
+    return {
+        qt: quantity.qt * coef(quantity.unit),
+        unit: base(grandeur(quantity.unit)).shortname
+    };
+};
+
+var unitsFromShortname = function unitsFromShortname(shortname) {
+    return grandeurs[unit(shortname).grandeur];
+};
+var bestQuantity = exports.bestQuantity = function bestQuantity(quantity) {
+    var units = unitsFromShortname(quantity.unit);
+    var currentUnit = unit(quantity.unit);
+    var currentUnitIndex = _lodash2.default.findIndex(units, { shortname: quantity.unit });
+    if (currentUnitIndex < units.length - 1) {
+        var upperUnit = units[currentUnitIndex + 1];
+        var uppingCoef = upperUnit.coef / currentUnit.coef;
+        if (quantity.qt >= uppingCoef) {
+            return bestQuantity({ qt: quantity.qt / uppingCoef, unit: upperUnit.shortname });
+        }
+    }
+    if (currentUnitIndex > 0) {
+        var lowerUnit = units[currentUnitIndex - 1];
+        var downingCoef = lowerUnit.coef / currentUnit.coef;
+        if (quantity.qt <= downingCoef) {
+            return bestQuantity({ qt: quantity.qt / downingCoef, unit: lowerUnit.shortname });
+        }
+    }
+
+    return { qt: bestRound(quantity.qt), unit: quantity.unit };
 };
--- src/units.js
+++ src/units.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
+let data = null;
+let units = null;
+let grandeurs = null;
+
 export const initUnits = grandeurs => {
 
     _.forEach(grandeurs, (units, grandeurName) => {
@@ -7,13 +11,91 @@ export const initUnits = grandeurs => {
         grandeurs[grandeurName] = _.sortBy(units, 'coef');
     });
 
-    const units = _.chain(grandeurs).values().flatten().keyBy('shortname').value();
+    units = _.chain(grandeurs).values().flatten().keyBy('shortname').value();
 
-    return {
+    this.grandeurs = grandeurs;
+
+    data = {
         units,
         grandeurs,
         grandeursKeys: Object.keys(grandeurs),
         shortnames: Object.keys(units)
     };
 
+};
+
+
+export const getUnits = () => data.units;
+export const getGrandeurs = () => data.grandeurs;
+export const getGrandeursKeys = () => data.grandeursKeys;
+export const getShortnames = () => data.shortnames;
+
+
+export const unit = shortname => _.has(units, shortname) ? units[shortname] : null;
+export const coef = shortname => unit(shortname).coef;
+export const base = grandeur => _.find(grandeurs[grandeur], {coef: 1});
+
+export const unitlongname = shortname => unit(shortname).name;
+export const grandeur = shortname => unit(shortname).grandeur;
+
+/**
+ * @returns faux, ou vrai ssi les unités sont valides et de la même grandeur
+ */
+export const sameGrandeur = (leftShortname, rightShortname) => {
+    const leftUnit = unit(leftShortname);
+    const rightUnit = unit(rightShortname);
+
+    return leftUnit && rightUnit && leftUnit.grandeur === rightUnit.grandeur;
+
+};
+
+/**
+ * @returns le coef pour passer d'une unité à l'autre. undefined si les unités ne sont pas compatibles.
+ */
+export const unitCoef = (leftShortname, rightShortname) => sameGrandeur(leftShortname, rightShortname)
+    ? Fraction(unit(leftShortname).coef).div(unit(rightShortname).coef).valueOf()
+    : undefined;
+
+/**
+ * @returns le coef pour passer d'une quantité à l'autre. undefined si les unités ne sont pas compatibles.
+ */
+export const qtUnitCoef = (leftQuantity, rightQuantity) => leftQuantity && rightQuantity
+    ? Fraction(leftQuantity.qt)
+        .div(rightQuantity.qt)
+        .mul(unitCoef(leftQuantity.unit, rightQuantity.unit)).valueOf()
+    : undefined;
+
+/**
+ *
+ * @param quantity
+ * @returns la quantité en unité de base. (10kg => 10000g)
+ */
+export const toBaseQuantity = quantity => {
+    return {
+        qt: quantity.qt * coef(quantity.unit),
+        unit: base(grandeur(quantity.unit)).shortname
+    };
+};
+
+const unitsFromShortname = shortname => grandeurs[unit(shortname).grandeur];
+export const bestQuantity = (quantity) => {
+    const units = unitsFromShortname(quantity.unit);
+    const currentUnit = unit(quantity.unit);
+    const currentUnitIndex = _.findIndex(units, {shortname: quantity.unit});
+    if (currentUnitIndex < units.length - 1) {
+        const upperUnit = units[currentUnitIndex + 1];
+        const uppingCoef = upperUnit.coef / currentUnit.coef;
+        if (quantity.qt >= uppingCoef) {
+            return bestQuantity({qt: quantity.qt / uppingCoef, unit: upperUnit.shortname});
+        }
+    }
+    if (currentUnitIndex > 0) {
+        const lowerUnit = units[currentUnitIndex - 1];
+        const downingCoef = lowerUnit.coef / currentUnit.coef;
+        if (quantity.qt <= downingCoef) {
+            return bestQuantity({qt: quantity.qt / downingCoef, unit: lowerUnit.shortname});
+        }
+    }
+
+    return {qt: bestRound(quantity.qt), unit: quantity.unit};
 };
